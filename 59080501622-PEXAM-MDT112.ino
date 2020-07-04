c1ff04447fb9af81e4e9e4b99a1c827aa54cec15
monmos339@gmail.com <monmos339@gmail.com>
2020-07-04 13:32:29 +0700

    3.Complete

--- 59080501622-PEXAM-MDT112.ino
+++ 59080501622-PEXAM-MDT112.ino
@@ -1,10 +1,22 @@
+const int MTR1 = 5;
+const int MTL1 = 6;
+const int MTR2 = 9;
+const int MTL2 = 10;
+
 void setup(){
     Serial.begin(9600);
     pinMode(8,OUTPUT);
     tone(8,400,800);
     Serial.println("Hello MDT !!")
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
 }
 
 void loop(){
-
+    Serial.println("Turn CCW");
+    analogWrite(MTL1,1);
+    analogWrite(MTR1,0);
+    delay(3000);
+    analogWrite(MTL1,0);
+    analogWrite(MTR1,0);
 }
