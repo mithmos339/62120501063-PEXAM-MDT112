99f211ba36c0146144dc70410c6d56aecc1b3c89
monmos339@gmail.com <monmos339@gmail.com>
2020-07-04 13:44:02 +0700

    5.Complete

--- 59080501622-PEXAM-MDT112.ino
+++ 59080501622-PEXAM-MDT112.ino
@@ -1,10 +1,17 @@
+#include "TM1637.h"
+
 const int MTR1 = 5;
 const int MTL1 = 6;
 const int MTR2 = 9;
 const int MTL2 = 10;
 
+const int CLK = 15;
+const int DIO = 14;
+
+TM1637 sevenSegment(CLK, DIO);
+
 void setup(){
-    Serial.begin(9600);
+    sevenSegment.init();
     pinMode(8,OUTPUT);
     tone(8,400,800);
     Serial.println("Hello MDT !!")
@@ -12,6 +19,7 @@ void setup(){
     pinMode(MTL1,OUTPUT);
     pinMode(MTR2,OUTPUT);
     pinMode(MTL2,OUTPUT);
+    Serial.begin(9600);
 
 }
 
@@ -31,4 +39,8 @@ void loop(){
     analogWrite(MTL2,0);
     analogWrite(MTR2,0);
     
+    sevenSegment.displayNum(0000);
+    delay(600);
+    sevenSegment.clear();
+
 }
